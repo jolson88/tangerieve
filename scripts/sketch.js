4edab987288b4633d3a9afea5917f9eca5f64ad2
Jason Olson <jolson88@outlook.com>
2020-10-18 16:38:35 -0700

    Enables Intellisense in Code for p5.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,8 +1,10 @@
+/// <reference path="../node_modules/@types/p5/global.d.ts" />
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(200, 200);
 }
 
 function draw() {
-  background(220);
+  background(255);
   ellipse(50,50,80,80);
 }
