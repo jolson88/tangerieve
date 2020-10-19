3aa882c974453e0291a2c9ee4e68a9fd67e27115
Jason Olson <jolson88@outlook.com>
2020-10-18 17:39:54 -0700

    Explores simple circles

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,10 +1,24 @@
 /// <reference path="../node_modules/@types/p5/global.d.ts" />
 
+const size = 600;
+const colorLookup = [20, 90, 160, 230];
+
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(size, size);
 }
 
 function draw() {
-  background(255);
-  ellipse(50,50,80,80);
+  background(230);
+
+  loadPixels();
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      const i = (x + 1) + ((x + 1) / size);
+      const j = (y + 1) + ((y + 1) / size); 
+      const v = floor(i * i + j * j);
+      set(x, y, colorLookup[v % 4]);
+    }
+  }
+  updatePixels();
+  noLoop();
 }
