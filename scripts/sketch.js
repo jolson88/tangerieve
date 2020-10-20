f70c10eb0cd9c9d71807deff6a34d0744eca6b53
Jason Olson <jolson88@outlook.com>
2020-10-20 12:39:02 -0700

    Adds configurable # of gradient colors

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,10 +1,16 @@
 /// <reference path="../node_modules/@types/p5/global.d.ts" />
 
 const size = 600;
-const colorLookup = [20, 90, 160, 230];
+const gradients = 12;
+let colorLookup = [];
 
 function setup() {
   createCanvas(size, size);
+  const colorRange = 180;
+  const colorStride = colorRange / gradients;
+  for (let i = 0; i < gradients; i++) {
+    colorLookup.push(i * colorStride + 50);
+  }
 }
 
 function draw() {
@@ -16,7 +22,7 @@ function draw() {
       const i = (x + 1) + ((x + 1) / size);
       const j = (y + 1) + ((y + 1) / size); 
       const v = floor(i * i + j * j);
-      set(x, y, colorLookup[v % 4]);
+      set(x, y, colorLookup[v % gradients]);
     }
   }
   updatePixels();
